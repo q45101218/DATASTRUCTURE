cf72acae6e882b03cd3b54f4bedff2af5b58e9c9
Yangkun <45101218@qq.com>
2017-11-05 06:25:55 -0800

    hashtable

--- Hashtable/Hashtable.cpp
+++ Hashtable/Hashtable.cpp
@@ -19,9 +19,25 @@ enum condition
 template<class K,class V>
 struct Hashnode
 {
-    pair<K,V> member;
+    K key;
+    V value;
     condition con;
+
+    Hashnode(const K& k=0,const V& v=0)
+    :key(k)
+    ,value(v)
+    ,con(EMPTY)
+    {}
 };
+
+template<class K,class V>
+struct HashFun
+{
+    size_t operator()(num,size)
+    {
+        return num%size;
+    }
+}
 
 template<class K,class V,class __HashFun=HashFun>
 class Hashtable
@@ -29,8 +45,43 @@ class Hashtable
 public:
     typedef Hashnode<K,V> node;
 
+    Hashtable()
+    :size(0)
+    {}
+
+    bool Insert(K& k,V& v)
+    {
+        Checkconflict();
+        __HashFun HashFun;
+        size_t index=HashFun(k,size);
+        while()
+        {
+            if(table[index].con!=EXIST)
+            {
+                table[index].key=k;
+                table[index].value=v;
+                table[index].con=EXIST;
+                break;
+            }
+            index++;
+            if(index>table.size())
+            {
+                index=0;
+            }
+        }
+    }
+
+    void Checkconflict()
+    {
+        if(size=0||size*10%table.size()>=7)
+        {
+            size==0? table.resize(7):table.resize(size*2);
+        }
+    }
+
+
 private:
-    vector<node> hashtable;
+    vector<node> table;
     size_t size;
 };
 
