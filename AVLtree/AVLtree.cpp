e1efa75ce6c8058d877ae3bc6161021215686871
Yangkun <45101218@qq.com>
2017-11-14 12:27:24 -0800

    avl

--- AVLtree/AVLtree.cpp
+++ AVLtree/AVLtree.cpp
@@ -11,11 +11,30 @@ using namespace std;
 
 template<class T>
 struct Treenode
-{};
+{
+    Treenode<T>* _right;
+    Treenode<T>* _left;
+    Treenode<T>* _parent;
+    int _balance;
+    T _data;
+
+    Treenode(const T& data)
+    :_data(data)
+    {}
+};
 
 template<class T>
 class AVLTree
-{};
+{
+    typedef Treenode<T> node;
+public:
+    AVLTree()
+    :_root(NULL)
+    {}
+
+private:
+    node* _root;
+};
 
 int main()
 {
