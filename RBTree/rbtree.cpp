f4c6342452d6896d803ebacc5ddd46a2fc7a8e01
Yangkun <45101218@qq.com>
2017-11-13 17:27:05 -0800

    add rbtree

---
+++ RBTree/rbtree.cpp
@@ -0,0 +1,22 @@
+/*************************************************************************
+	> File Name: rbtree.cpp
+	> Author:YangKun 
+	> Mail:yangkungetit@163.com 
+	> Created Time: Mon 13 Nov 2017 05:25:07 PM PST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+template<class T>
+struct Treenode
+{};
+
+template<class T>
+class RBtree
+{};
+
+int main()
+{
+    return 0;
+}
