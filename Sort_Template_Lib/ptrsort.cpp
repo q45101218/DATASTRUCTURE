084b0210306c7d03127f9236b19684a59e153e40
Yangkun <45101218@qq.com>
2017-11-23 13:59:06 +0800

    ptrsort

--- Sort_Template_Lib/ptrsort.cpp
+++ Sort_Template_Lib/ptrsort.cpp
@@ -8,48 +8,53 @@
 #include<iostream>
 using namespace std;
 
+template<class T>
 struct Greater
 {
-    bool operator()(int x,int y)
+    bool operator()(const T& x,const T& y)
     {
         return x>=y;
     }
 };
 
+template<class T>
 struct Less 
 {
-    bool operator()(int x,int y)
+    bool operator()(const T& x,const T& y)
     {
         return x<=y;
     }
 };
 
-template<class Compare>
+template<template<class>class Compare=Greater,class T=int>
 void ptrsort(int* data,int begin,int end)
 {
-    Compare com;
+    if(begin>=end)
+        return;
+
+    Compare<T> com;
     int slow=begin-1;
     int fast=begin;
 
-    while()
+    while(fast<end)
     {
-        while(fast=<end && com(data[fast],))
+        if(com(data[end],data[fast]))
         {
-            fast++;
+            swap(data[++slow],data[fast]);
         }
-        while()
-        {
-            slow++;
-        }
-        swap(data[fast],data[slow]);
+        fast++;
     }
+    swap(data[end],data[++slow]);
+
+    ptrsort<Compare,T>(data,begin,slow-1);
+    ptrsort<Compare,T>(data,slow+1,end);
 
 }
 
 int main()
 {
     int data[] = { 2, 5, 4, 9, 3, 6, 8, 7, 1, 0  };
-    ptrsort<Less>(data, 0, sizeof(data) / sizeof(data[0]) - 1);
+    ptrsort<Less,int>(data, 0, sizeof(data) / sizeof(data[0]) - 1);
     ptrsort(data, 0, sizeof(data) / sizeof(data[0]) - 1);
     return 0;
 }
