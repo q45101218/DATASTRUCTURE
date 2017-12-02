bf191813fac0daba3e4948f770df1ffb3724eb74
yangkun <45101218@qq.com>
2017-12-02 22:49:42 +0800

    add iterator

--- List/list.hpp
+++ List/list.hpp
@@ -24,13 +24,29 @@ struct Node
     T _data;
 };
 
+template<class T,class Ref,class Ptr>
+struct Iterator
+{
+    typedef Iterator<T,Ref,Ptr> Self;
+    typedef Node<T> node;
+    Iterator(node* p=NULL)
+    :_ptr(p)
+    {}
+
+
+    node* _ptr;
+}
+
 template<class T>
 class List
 {
 public:
     List()
-    :_head(NULL)
-    {}
+    :_head(new node)
+    {
+        _head->_prev=_head;
+        _head->_next=_head;
+    }
 
     List(const List& l)
     :_head(NULL)
