b3af1790a588859473336787a0b8e4c70b86d273
yangkun <45101218@qq.com>
2017-12-24 23:27:18 +0800

    bin tree

---
+++ BinarySearch/binarytree.cpp
@@ -0,0 +1,67 @@
+/*************************************************************************
+	> File Name: binarytree.cpp
+	> Author:yangkun 
+	> Mail:yangkungetit@163.com 
+	> Created Time: Sun 24 Dec 2017 10:30:49 PM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+template<class T>
+struct Treenode
+{
+    T _data;
+    Treenode<T>* _leftchild;
+    Treenode<T>* _rightchild;
+
+    Treenode(const T& data)
+    :_data(data)
+    ,_leftchild(NULL)
+    ,_rightchild(NULL)
+    {}
+};
+
+template<class T>
+class binarytree
+{
+    typedef Treenode<T> node;
+public:
+    binarytree(const T* data,int value)
+    {
+        int index=0;
+        _root=createtree(data,index,value);
+    }
+
+    node* createtree(const T* data,int& index,int value)
+    {
+        if(data[index]==value)
+            return NULL;
+        else
+        {
+            node* tmp=new node(data[index]);
+            tmp->_leftchild=createtree(data,++index,value);
+            tmp->_rightchild=createtree(data,++index,value);
+            return tmp;
+        }
+    }
+
+private:
+    node* _root;
+};
+
+
+int test()
+{
+    int arry[]={1,2,3,'#','#',4,'#','#',5,6,'#','#','#'};
+    binarytree<int> bt(arry,'#');
+    int i;
+    return i;
+}
+
+int main()
+{
+    test();
+    return 0;
+}
+
