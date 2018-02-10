09853746c89b0300e7c0d8a9249ef4167eb03df5
yangkun <45101218@qq.com>
2018-02-10 21:55:16 +0800

    fix

--- AVLtree/AVLtree.cpp
+++ AVLtree/AVLtree.cpp
@@ -20,6 +20,10 @@ struct Treenode
 
     Treenode(const T& data)
     :_data(data)
+    ,_balance(0)
+    ,_left(NULL)
+    ,_right(NULL)
+    ,_parent(NULL)
     {}
 };
 
@@ -31,6 +35,103 @@ public:
     AVLTree()
     :_root(NULL)
     {}
+
+    void preorder()
+    {
+        _preorder( _root);
+    }
+
+    void midorder()
+    {
+        _midorder( _root);
+    }
+
+    void postorder()
+    {
+        _postorder( _root);
+    }
+
+    bool insert(T& data)
+    {
+        if(_root == NULL)
+        {
+            _root = new node(data);
+            return true;
+        }
+        node* cur = _root;
+        while(cur)
+        {
+            if(data < cur->_data)
+            {
+                cur = cur->_left;
+            }
+            else if(data > cur->_data)
+            {
+                cur = cur->_right;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+
+
+private:
+
+    void _preorder(node*& const root)
+    {
+        if(root == NULL)
+        {
+            return;
+        }
+        cout << root->_data << endl;
+        if( root->_left != NULL )
+        {
+            _preorder(root->_left);
+        }
+        
+        if( root->_right != NULL )
+        {
+            _preorder(root->_right);
+        }
+    }
+
+    void _midorder(node*& const root)
+    {
+        if(root == NULL)
+        {
+            return;
+        }
+
+        if(root->_left != NULL)
+        {
+            _midorder(root->_left);
+        }
+        cout << root->_data << endl;
+        if(root->_right != NULL)
+        {
+            _midorder(root->_right);
+        }
+    }
+
+    void _postorder(node*& const root)
+    {
+        if(root == NULL)
+        {
+            return;
+        }
+        if(root->_left != NULL)
+        {
+            _postorder(root->_left);
+        }
+        if(root->_right != NULL)
+        {
+            _postorder(root->_right);
+        }
+        cout << root->_data << endl;
+    }
 
 private:
     node* _root;
